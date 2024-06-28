1951dea71d3246f2b0db2235dac6ed1a0471e15a
Gal Kepler <galkepler@gmail.com>
2024-06-28 13:04:35 +0300

    passing mypy errors

--- src/yalab_procedures/procedure/procedure.py
+++ src/yalab_procedures/procedure/procedure.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from abc import ABC, abstractmethod
+from datetime import datetime
 from pathlib import Path
 from typing import Optional, Union
 
@@ -26,25 +27,25 @@ class Procedure(ABC):
     def __init__(
         self,
         inputs: Union[str, Path],
-        config: Union[dict, str, Path] = None,
-        outputs_destination: Optional[Union[str, Path]] = None,
+        outputs_destination: Union[str, Path],
+        config: Optional[Union[dict[str, str], str, Path]] = None,
         logging_destination: Optional[Union[str, Path]] = None,
         logging_level: str = "INFO",
     ):
         self.inputs = Path(inputs)
         self.config = self._load_config(config)
-        self.outputs_destination = (
-            Path(outputs_destination) if outputs_destination else None
-        )
+        self.outputs_destination = Path(outputs_destination)
         self.logging_destination = (
-            Path(logging_destination) if logging_destination else None
+            Path(logging_destination)
+            if logging_destination
+            else self.outputs_destination
         )
         self.logging_level = logging_level
         self.logger = logging.getLogger(self.__class__.__name__)
         self._setup_logging()
 
     @abstractmethod
-    def run(self):
+    def run(self) -> None:
         """
         Abstract method to run the procedure.
         """
@@ -52,12 +53,16 @@ class Procedure(ABC):
             "This is the abstract method for the Procedure class. It should be implemented in the child class."
         )
 
-    def _setup_logging(self):
+    def _setup_logging(self) -> None:
         """
         Sets up logging configuration.
         """
+        handler: logging.Handler
         if self.logging_destination:
-            handler = logging.FileHandler(self.logging_destination)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            log_filename = f"{self.__class__.__name__}_{timestamp}.log"
+            log_file_path = self.logging_destination / log_filename
+            handler = logging.FileHandler(log_file_path)
         else:
             handler = logging.StreamHandler()
         formatter = logging.Formatter(
@@ -67,7 +72,7 @@ class Procedure(ABC):
         self.logger.addHandler(handler)
         self.logger.setLevel(getattr(logging, self.logging_level.upper(), "INFO"))
 
-    def log(self, message: str):
+    def log(self, message: str) -> None:
         """
         Logs a message at the INFO level.
 
@@ -78,7 +83,9 @@ class Procedure(ABC):
         """
         self.logger.info(message)
 
-    def _load_config(self, config: Union[dict, str, Path, None]) -> Optional[dict]:
+    def _load_config(
+        self, config: Union[dict[str, str], str, Path, None]
+    ) -> dict[str, str]:
         """
         Loads the configuration from a file or dictionary.
 
@@ -98,5 +105,5 @@ class Procedure(ABC):
             config_path = Path(config)
             if config_path.is_file():
                 with open(config_path, "r") as file:
-                    return json.load(file)
-        return None
+                    return dict(json.load(file))
+        return {}
