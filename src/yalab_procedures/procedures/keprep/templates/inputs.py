1db2e41cd905fcda68ba9bf13ed0363306b72847
GalKepler <galkepler@gmail.com>
2024-07-18 18:26:38 +0300

    WIP keprep procedure

---
+++ src/yalab_procedures/procedures/keprep/templates/inputs.py
@@ -0,0 +1,10 @@
+"""
+Mapping of input parameters to the KepRep configuration file.
+"""
+
+INPUTS_MAPPING = {
+    "input_directory": "bids_dir",
+    "output_directory": "output_dir",
+    "work_directory": "work_dir",
+    "logging_directory": "log_dir",
+}
