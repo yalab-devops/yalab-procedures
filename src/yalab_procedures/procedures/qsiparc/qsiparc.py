756df3e48481a88bb5600183db1f3b6d5024d128
GalKepler <galkepler@gmail.com>
2025-12-01 13:39:11 +0200

    added qsiparc procedure

---
+++ src/yalab_procedures/procedures/qsiparc/qsiparc.py
@@ -0,0 +1,249 @@
+import logging
+import os
+from pathlib import Path
+from subprocess import CalledProcessError, run
+from typing import Any, Dict
+
+from nipype.interfaces.base import (
+    CommandLineInputSpec,
+    Directory,
+    File,
+    isdefined,
+    traits,
+)
+from parcellate.interfaces.qsirecon.qsirecon import QSIReconConfig, run_parcellations
+
+from yalab_procedures.procedures.base.procedure import (
+    Procedure,
+    ProcedureInputSpec,
+    ProcedureOutputSpec,
+)
+
+
+class QsiparcInputSpec(ProcedureInputSpec, CommandLineInputSpec):
+    """
+    Input specification for the QsiparcProcedure
+    """
+
+    input_directory = Directory(
+        exists=True,
+        mandatory=True,
+        desc="Input directory containing QSIRecon BIDS dataset",
+    )
+    temporary_bids_directory = Directory(
+        exists=False,
+        mandatory=False,
+        desc="Temporary BIDS directory",
+    )
+    output_directory = Directory(
+        exists=False,
+        mandatory=True,
+        desc="Directory to store Qsiparc's output",
+    )
+    work_directory = Directory(
+        exists=False,
+        mandatory=True,
+        desc="Path to work directory",
+    )
+    participant_label = traits.List(
+        traits.Str,
+        desc="Participant label",
+        sep=",",
+    )
+    resampling_target = traits.Enum(
+        "data",
+        "atlas",
+        "labels",
+        desc="Resampling target for parcellation",
+    )
+    mask = traits.Str(
+        "gm",
+        usedefault=True,
+        desc="Type of mask to use for parcellation (e.g., 'gm' for gray matter)",
+    )
+    skip_bids_validation = traits.Bool(
+        True,
+        desc="Skip BIDS validation",
+    )
+    nprocs = traits.Int(
+        os.cpu_count(),
+        usedefault=True,
+        desc="Number of processes (compute tasks) that can be run in parallel (multiprocessing only).",
+    )
+    omp_nthreads = traits.Int(
+        1,
+        usedefault=True,
+        desc="Number of CPUs a single process can access for multithreaded execution.",
+    )
+    force = traits.Bool(
+        False,
+        usedefault=True,
+        desc="Whether to force the procedure to run even if the output directory already exists.",
+    )
+
+
+class QsiparcOutputSpec(ProcedureOutputSpec):
+    """
+    Output specification for the QsiparcProcedure
+    """
+
+    output_directory = Directory(
+        exists=True,
+        desc="Directory where Qsiparc outputs are stored",
+    )
+    log_file = File(
+        exists=True,
+        desc="Qsiparc log file",
+    )
+
+
+class QsiparcProcedure(Procedure):
+    """
+    Procedure for running Qsiparc
+    """
+
+    input_spec = QsiparcInputSpec
+    output_spec = QsiparcOutputSpec
+    _version = "0.0.1"
+
+    def __init__(self, **inputs: Any):
+        super().__init__(**inputs)
+
+    def _get_default_value(self, key: str) -> Any:
+        """
+        Get the default value of an input
+        """
+        value = getattr(self.inputs, key)
+        return value if isdefined(value) else self.inputs.traits().get(key).default
+
+    def run_procedure(self, **kwargs):
+        """
+        Run the QsiparcProcedure
+        Raises
+        ------
+        CalledProcessError
+            If the command fails to run. The error message will be logged.
+        """
+        self.setup_logging()
+        self.logger.info("Running QsiparcProcedure")
+        self.logger.debug(f"Input attributes: {kwargs}")
+
+        if not self.inputs.force:
+            self.logger.info(
+                f"Attempting to locate outputs from previous run in {self.inputs.output_directory}"
+            )
+            result = self._list_outputs()
+            if all(Path(value).exists() for value in result.values()):
+                self.logger.info(
+                    f"Outputs already exist in {self.inputs.output_directory}. If you want to run the procedure again, set force=True."
+                )
+                return
+        finished_file, proceed = self._check_old_runs_finished()
+        if not proceed:
+            self.logger.info(
+                f"Previous run detected as finished in {self.inputs.output_directory}. If you want to run the procedure again, set force=True."  # noqa: E501
+            )
+            return
+        # Prepare inputs
+        temp_input_directory = self._prepare_inputs()
+        # Run the qsiprep command
+        config = self._initiate_config()
+        try:
+            _ = run_parcellations(config)
+        except Exception as e:
+            self.logger.error(f"QsiparcProcedure failed with error: {e}")
+            raise CalledProcessError(
+                returncode=1,
+                cmd="run_parcellations",
+                output=str(e),
+            ) from e
+        self.logger.info("Finished running QSIPrepProcedure")
+        self.logger.info(
+            f"Cleaning up temporary input directory: {temp_input_directory}"
+        )
+        # Clean up
+        result = run(
+            f"rm -rf {temp_input_directory}",
+            shell=True,
+            check=False,
+            capture_output=True,
+            text=True,
+        )
+        if result.returncode != 0:
+            self.logger.warning(
+                f"Failed to remove temporary input directory: {temp_input_directory}. Error: {result.stderr}"  # noqa: E501
+            )
+        self._write_finished_file(finished_file)
+
+    def _prepare_inputs(self):
+        """
+        Prepare inputs for the QsiprepProcedure
+        """
+        work_directory = Path(self.inputs.work_directory)
+        input_directory = Path(self.inputs.input_directory)
+        temporary_bids_directory = self.inputs.temporary_bids_directory
+        if isdefined(temporary_bids_directory):
+            temp_bids = Path(temporary_bids_directory)
+            temp_bids.mkdir(parents=True, exist_ok=True)
+        else:
+            temp_bids = work_directory
+        # generate random temporary directory
+        temp_bids = temp_bids / f"qsiparc_temp_bids_{os.getpid()}"
+        self.logger.info(f"Using provided temporary BIDS directory: {temp_bids}")
+        temp_bids.mkdir(parents=True, exist_ok=True)
+        # rsync input directory to work directory
+        for participant in self.inputs.participant_label:
+            run(
+                f"rsync -azPL {input_directory}/sub-{participant} {temp_bids}",
+                shell=True,
+                check=True,
+            )
+            for derivatives in (input_directory / "derivatives").glob(
+                f"qsirecon-*/sub-{participant}"
+            ):
+                run(
+                    f"rsync -azPL --exclude='*.tck*' --exclude='*.trk*' {derivatives} {temp_bids / 'derivatives'}",
+                    shell=True,
+                    check=True,
+                )
+                run(
+                    f"rsync -azPL {derivatives.parent}/dataset_description.json {temp_bids / 'derivatives'}",
+                    shell=True,
+                    check=True,
+                )
+        for fname in ["dataset_description.json", "atlases"]:
+            run(
+                f"rsync -azPL {input_directory / fname} {temp_bids}",
+                shell=True,
+                check=True,
+            )
+        self.inputs.input_directory = temp_bids
+        return temp_bids
+
+    def _list_outputs(self) -> Dict[str, str]:
+        """
+        List the outputs of the QsiparcProcedure
+        """
+        output_directory = Path(self.inputs.output_directory)
+        if output_directory.name != "qsiparc":
+            output_directory = output_directory / "qsiparc"
+        outputs = self._outputs().get()
+        outputs["output_directory"] = str(output_directory)
+        if hasattr(self, "log_file_path"):
+            outputs["log_file"] = str(self.log_file_path)
+        return outputs
+
+    def _initiate_config(self) -> QSIReconConfig:
+        """
+        Initialize QSIReconConfig from inputs
+        """
+        config = QSIReconConfig(
+            input_root=Path(self.inputs.input_directory),
+            output_dir=Path(self.inputs.output_directory),
+            subjects=self.inputs.participant_label,
+            resampling_target=self.inputs.resampling_target,
+            force=self.inputs.force,
+            log_level=logging.DEBUG,
+            mask=self.inputs.mask,
+        )
+        return config
---
+++ src/yalab_procedures/procedures/qsiparc/templates/outputs.py
@@ -0,0 +1,67 @@
+# flake8: noqa: E501
+QSIPREP_OUTPUTS = {
+    "qsiprep": {
+        # T1w-related outputs
+        "preprocessed_T1w": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-preproc_T1w.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_desc-preproc_T1w.nii.gz",
+        },
+        "brain_mask": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-brain_mask.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_desc-brain_mask.nii.gz",
+        },
+        "MNI_preprocessed_T1w": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz",
+        },
+        "MNI_brain_mask": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
+        },
+        # Transformations
+        "mni_to_native_transform": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_from-MNI152NLin2009cAsym_to-T1w_mode-image_xfm.h5",
+            "subject": "sub-{subject}/anat/sub-{subject}_from-MNI152NLin2009cAsym_to-T1w_mode-image_xfm.h5",
+        },
+        "native_to_mni_transform": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_from-T1w_to-MNI152NLin2009cAsym_mode-image_xfm.h5",
+            "subject": "sub-{subject}/anat/sub-{subject}_from-T1w_to-MNI152NLin2009cAsym_mode-image_xfm.h5",
+        },
+        "fsnative_to_native_transform": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_from-fsnative_to-T1w_mode-image_xfm.txt",
+            "subject": "sub-{subject}/anat/sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.txt",
+        },
+        "native_to_fsnative_transform": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_from-T1w_to-fsnative_mode-image_xfm.txt",
+            "subject": "sub-{subject}/anat/sub-{subject}_from-T1w_to-fsnative_mode-image_xfm.txt",
+        },
+        # Segmentation outputs
+        "segmentation": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_desc-aparcaseg_dseg.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_desc-aparcaseg_dseg.nii.gz",
+        },
+        # Probabilistic segmentation outputs
+        "probseg_gm": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_label-GM_probseg.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_label-GM_probseg.nii.gz",
+        },
+        "probseg_wm": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_label-WM_probseg.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_label-WM_probseg.nii.gz",
+        },
+        "probseg_csf": {
+            "session": "sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_label-CSF_probseg.nii.gz",
+            "subject": "sub-{subject}/anat/sub-{subject}_label-CSF_probseg.nii.gz",
+        },
+        # Add other necessary output paths as needed
+    },
+    "freesurfer": {
+        "fsaverage": "fsaverage/mri/brain.mgz",
+        "T1w": "sub-{subject}/mri/T1.mgz",
+        "brainmask": "sub-{subject}/mri/brainmask.mgz",
+        "brain": "sub-{subject}/mri/brain.mgz",
+        "wm": "sub-{subject}/mri/wm.mgz",
+        "lh_pial": "sub-{subject}/surf/lh.pial",
+        "rh_pial": "sub-{subject}/surf/rh.pial",
+    },
+}
